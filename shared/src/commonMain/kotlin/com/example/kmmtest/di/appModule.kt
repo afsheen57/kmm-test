f396e74990813c9e24ec0ffe721d05988a42b7bd
aderivanto <ade.rivanto@phincon.com>
2024-01-03 18:23:19 +0700

    update koin

--- shared/src/commonMain/kotlin/com/example/kmmtest/di/appModule.kt
+++ shared/src/commonMain/kotlin/com/example/kmmtest/di/appModule.kt
@@ -1,7 +1,14 @@
 package com.example.kmmtest.di
 
 import com.example.kmmtest.network.HttpClient
+import org.koin.core.context.startKoin
 import org.koin.dsl.module
+
+fun initKoin() {
+    startKoin {
+        modules(commonModule)
+    }
+}
 
 val commonModule = module {
     single { HttpClient() }
